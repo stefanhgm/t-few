17fc00f266b88e51018db4901ee888b4f3786744
stefanhg <stefan@hegselmann.com>
2022-06-29 10:13:10 -0400

    added external datasets

--- bin/experiments/few-shot-pretrained-3b-100k.sh
+++ bin/experiments/few-shot-pretrained-3b-100k.sh
@@ -1,12 +1,18 @@
 allow_skip_exp=True
 train_batch_size=8
-cuda_device=0
+
+# TODO: Set per experiment
+cuda_device=1
 
 # Set adaptively
 num_steps=0 #  #  2000, 256 / 1k: 5000 4k (30 epochs)/16k (8 epochs): 16000 all (5 epochs): 60000 eol 320000 loh/surgery | balanced: 4k shot eol, 20000 loh, 135000 surgery
 eval_epoch_interval=0
 
-for num_shot in 32 64 128 256 1024 4096 16348 50000 100000 800000  # 1024  # 16384 4096 1024 256  1000000
+# TODO: Set per experiment
+# Excluded 800000 for now since one epoch takes ~20h
+# IBC  32 64 128 256 1024 4096 16348 50000 100000 800000
+# External 32 64 128 256 512 1024 2048 4096 16348 50000
+for num_shot in 32 64 128 256 512 1024 2048 4096
 do
   # Determine epoch number xxx * for each shot size
   if [ "$num_shot" -le 64 ] ; then
@@ -30,9 +36,13 @@ do
     eval_epoch_interval=50
   fi
 
+# TODO: Set per experiment
   # income car
   # eol_important_v_c_10 eol_important_v_c_999 eol_important_v_c_10_balanced eol_important_v_c_999_balanced loh_important_v_c_10 loh_important_v_c_999 loh_important_v_c_10_balanced loh_important_v_c_999_balanced surgery_important_v_c_10 surgery_important_v_c_999 surgery_important_v_c_10_balanced surgery_important_v_c_999_balanced
-  for dataset in eol_important_v_c_p_999
+  # 2048: titanic titanic_list heart heart_list diabetes diabetes_list voting voting_list
+  # 4096: wine wine_list car car_list
+  # 50000: income income_list
+  for dataset in wine wine_list car car_list
   do
     for seed in 42  # 1024 0 1 32
     do
