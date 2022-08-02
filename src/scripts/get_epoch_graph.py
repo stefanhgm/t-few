4bdbfba9f0649eb6ee711b46cf08e8fa6385b54b
stefanhg <stefan@hegselmann.com>
2022-08-02 19:34:23 -0400

    Refactored.

--- src/scripts/get_epoch_graph.py
+++ src/scripts/get_epoch_graph.py
@@ -8,36 +8,39 @@ import numpy as np
 import os
 import argparse
 
+def get_epoch_wide_results(exp_name_template, datasets):
+    print("=" * 80)
+    all_files = glob(
+        os.path.join(os.getenv("OUTPUT_PATH", default="exp_out"), exp_name_template, "dev_scores.json")
+    )
+    print(f"Find {len(all_files)} experiments fit into {exp_name_template}")
+
+    def read_acc_per_epoch(fname):
+        results = []
+        with open(fname) as f:
+            for l in f.readlines():
+                e = json.loads(l)
+                results.append(e["AUC"])
+        return results
+
+    results = defaultdict(list)
+    all_files.sort(key=lambda x: (int(re.findall(r".*_numshot(\d+).*", x)[0]), x), reverse=True)
+    for fname in all_files:
+        if any([d in fname for d in datasets]):
+            name = fname.split('_seed')[0]
+            accs = read_acc_per_epoch(fname)
+            if (len(accs) not in [5, 81, 49]) or (len(results[name]) > 0 and len(results[name][0]) > len(accs)):
+                continue  # Ignore incomplete results
+            results[name] = results[name] + [read_acc_per_epoch(fname)]
+    return results
+
 
 def make_epoch_graph(args):
-    def collect_exp_scores(exp_name_template, datasets):
-        print("=" * 80)
-        all_files = glob(
-            os.path.join(os.getenv("OUTPUT_PATH", default="exp_out"), exp_name_template, "dev_scores.json")
-        )
-        print(f"Find {len(all_files)} experiments fit into {exp_name_template}")
-
-        def read_acc_per_epoch(fname):
-            results = []
-            with open(fname) as f:
-                for l in f.readlines():
-                    e = json.loads(l)
-                    results.append(e["AUC"])
-            return results
-
-        results = defaultdict(list)
-        all_files.sort(key=lambda x: (int(re.findall(r".*_numshot(\d+).*", x)[0]), x), reverse=True)
-        for fname in all_files:
-            if any([d in fname for d in datasets]):
-                name = fname.split('_seed')[0]
-                accs = read_acc_per_epoch(fname)
-                if (len(accs) not in [41, 81, 25, 49]) or (len(results[name]) > 0 and len(results[name][0]) > len(accs)):
-                    continue  # Ignore incomplete results
-                results[name] = results[name] + [read_acc_per_epoch(fname)]
-
+    def make_epoch_graph_per_template(exp_name_template, datasets):
+        results = get_epoch_wide_results(exp_name_template, datasets)
         # Determine means and sd for each dataset
         fig, ax = plt.subplots(figsize=(8, 6))
-        epoch_steps = 5
+        epoch_steps = 10
         epoch_result = 29
         print(f"Use the {epoch_result}th epoch for the results (indexed by 0 so should be +1).")
         for k, v in results.items():
@@ -48,15 +51,18 @@ def make_epoch_graph(args):
             ax.fill_between(epochs, (means - stds), (means + stds), alpha=.1)
             print(f"{k}: {means[epochs.index(epoch_result)] * 100:.2f} ({stds[epochs.index(epoch_result)] * 100:.2f})")
         plt.legend(loc='lower right')
-        plt.xlabel('steps of 5 epochs')
-        plt.ylabel('AUC')
+        plt.xlabel(f"steps of {epoch_steps} epochs")
+        if datasets[0] == 'car':
+            plt.ylabel('Macro AUC OVR')
+        else:
+            plt.ylabel('AUC')
         plt.ylim(ymin=0.35, ymax=1.)
         plt.tight_layout()
         plt.show()
         print()
 
     for exp_name_template in args.exp_name_templates:
-        collect_exp_scores(exp_name_template, args.datasets)
+        make_epoch_graph_per_template(exp_name_template, args.datasets)
 
 
 if __name__ == "__main__":
